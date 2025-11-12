4433321419a09a5470f75fd3350765e789143ec1
andrelsrn <andrel.srn@hotmail.com>
2025-11-12 14:04:46 -0300

    Simple query with @Query

--- src/main/java/com/andrenunes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/andrenunes/workshopmongo/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.andrenunes.workshopmongo.repository;
 import com.andrenunes.workshopmongo.domain.Post;
 import com.andrenunes.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,8 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post,String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0 , $options: 'i' } }")
+    List<Post> findByTitle(String text);
 
 }
